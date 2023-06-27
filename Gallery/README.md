7a1f63f65827629a4be938bd5a93989188eeddbe
bryanjiang117 <haozhoujiang@gmail.com>
2023-06-26 21:14:47 -0400

    Merged with Gallery
    Gallery:
    combined Images1.jsx and Images2.jsx to Row.jsx
    changed js mouse events to css hover + background-image
    changed images in Rows.jsx to a parameter that App.jsx gives to Rows.jsx
    implemented arrays (lists) to store and display images dynamically
    implemented ImageObject instead of urls
    
    Merged Popup into Gallery

--- Gallery/README.md
+++ Gallery/README.md
@@ -1,1 +1,1 @@
-# gallery
+# Photo-Gallery
--- src/Imageobject.js
+++
@@ -1,9 +0,0 @@
-export default class ImageObject {
-  constructor(url, title, year, desc, author) {
-    this.url = url;
-    this.title = title;
-    this.year = year;
-    this.desc = desc;
-    this.author = author;
-  }
-}
